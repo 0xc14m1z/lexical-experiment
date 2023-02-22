874066304f32529828e41551334e9672a56490e9
Morgan Dam <0xc14m1z@gmail.com>
2023-02-22 09:10:59 +0100

    Add reference image to be faster in trying copy-pasting

--- src/Application.tsx
+++ src/Application.tsx
@@ -4,6 +4,8 @@ export function Application() {
   return (
     <main className="container mx-auto px-4 py-8">
       <Editor />
+      <hr />
+      <img src="https://uploads-ssl.webflow.com/5eec7a9c4479b07a3f62c64c/5f4cf46e8b5cc23d3a0d2a37_HomeHero_ModernCommunication.png" />
     </main>
   );
 }
